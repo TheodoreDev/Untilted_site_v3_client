db4940bfdb8cff1077ba0d23d203693c6e8d7453
Théodore <theodore.delbove@gmail.fr>
2024-02-01 17:11:18 +0100

    add checkAutenticate + light and dark theme

--- src/componants/mainPage/mainPage.js
+++ src/componants/mainPage/mainPage.js
@@ -1,75 +1,79 @@
-import React from 'react';
+import React, {useState} from 'react';
 import CreateAccount from './createAccountForm/createAccountForm'
 import Login from './loginForm/loginForm'
 import './css/mainPage.css'
+import {useNavigate} from 'react-router-dom'
+import { loadState } from '../../helper/sessionStorage';
 
-class MainPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+function MainPage () {
+
+  const sessionkey1 = "user"
+  const user = loadState(sessionkey1, [0])
+  const navigate = useNavigate()
+
+  const [classes, setClasses] = useState({
+    switchCtn: 'switch',
+    switchCircle0: 'switch__circle',
+    switchCircle1: 'switch__circle switch__circle--t',
+    switchC1: 'switch__container',
+    switchC2: 'switch__container is-hidden',
+    aContainer: 'container a-container',
+    bContainer: 'container b-container',
+  });
+
+
+  function switchLogin() {
+    setClasses({
+        switchCtn: "switch is-txr",
+        switchCircle0: "switch__circle is-txr",
+        switchCircle1: "switch__circle switch__circle--t is-txr",
+        switchC1: "switch__container is-hidden",
+        switchC2: "switch__container",
+        aContainer: "container a-container is-txl",
+        bContainer: "container b-container is-txl is-z200",
+    })
+  }
+
+  function switchSignin() {
+    setClasses({
         switchCtn: 'switch',
         switchCircle0: 'switch__circle',
         switchCircle1: 'switch__circle switch__circle--t',
         switchC1: 'switch__container',
         switchC2: 'switch__container is-hidden',
         aContainer: 'container a-container',
-        bContainer: 'container b-container',
-    };
-
-    this.switchLogin = this.switchLogin.bind(this)
-    this.switchSignin = this.switchSignin.bind(this)
-  }
-
-  switchLogin() {
-    this.setState({
-        switchCtn: this.state.switchCtn + " is-txr",
-        switchCircle0: this.state.switchCircle0 + " is-txr",
-        switchCircle1: this.state.switchCircle1 + " is-txr",
-        switchC1: this.state.switchC1 + " is-hidden",
-        switchC2: this.state.switchC2 = "switch__container",
-        aContainer: this.state.aContainer + " is-txl",
-        bContainer: this.state.bContainer + " is-txl is-z200",
+        bContainer: 'container b-container'
     })
   }
 
-  switchSignin() {
-    this.setState({
-        switchCtn: this.state.switchCtn = 'switch',
-        switchCircle0: this.state.switchCircle0 = 'switch__circle',
-        switchCircle1: this.state.switchCircle1 = 'switch__circle switch__circle--t',
-        switchC1: this.state.switchC1 = 'switch__container',
-        switchC2: this.state.switchC2 = 'switch__container is-hidden',
-        aContainer: this.state.aContainer = 'container a-container',
-        bContainer: this.state.bContainer = 'container b-container'
-    })
+  if(user.username){
+    navigate('/home')
   }
 
-  render() {
-    return (
-        <div className="main">
-            <div className={this.state.aContainer} id="a-container">
-                <CreateAccount></CreateAccount>
-            </div>
-            <div className={this.state.bContainer} id="b-container">
-                <Login></Login>
-            </div>
-            <div className={this.state.switchCtn} id="switch-cnt">
-                <div className={this.state.switchCircle0}></div>
-                <div className={this.state.switchCircle1}></div>
-                <div className={this.state.switchC1} id="switch-c1">
-                    <h2 className="switch__title title">Welcome Back !</h2>
-                    <p className="switch__description description">To keep connected with us please login with your personal info</p>
-                    <button className="switch__button button switch-btn" onClick={this.switchLogin}>GO TO LOG IN</button>
-                </div>
-                <div className={this.state.switchC2} id="switch-c2">
-                    <h2 className="switch__title title">Hello Friend !</h2>
-                    <p className="switch__description description">Enter your personal details and start journey with us</p>
-                    <button className="switch__button button switch-btn" onClick={this.switchSignin}>GO TO SIGN UP</button>
-                </div>
-            </div>
-        </div>
-    );
-  }
+  return (
+      <div className="main">
+          <div className={classes.aContainer} id="a-container">
+              <CreateAccount></CreateAccount>
+          </div>
+          <div className={classes.bContainer} id="b-container">
+              <Login></Login>
+          </div>
+          <div className={classes.switchCtn} id="switch-cnt">
+              <div className={classes.switchCircle0}></div>
+              <div className={classes.switchCircle1}></div>
+              <div className={classes.switchC1} id="switch-c1">
+                  <h2 className="switch__title title">Welcome Back !</h2>
+                  <p className="switch__description description">To keep connected with us please login with your personal info</p>
+                  <button className="switch__button button switch-btn" onClick={switchLogin}>GO TO LOG IN</button>
+              </div>
+              <div className={classes.switchC2} id="switch-c2">
+                  <h2 className="switch__title title">Hello Friend !</h2>
+                  <p className="switch__description description">Enter your personal details and start journey with us</p>
+                  <button className="switch__button button switch-btn" onClick={switchSignin}>GO TO SIGN UP</button>
+              </div>
+          </div>
+      </div>
+  );
 }
 
 export default MainPage;
--- src/componants/navBar/navBar.js
+++ src/componants/navBar/navBar.js
@@ -19,24 +19,33 @@ function NavBar() {
     const user = loadState(sessionkey1, [0])
 
     const [classes, setClasses] = useState({
-        logout_popup_active: ' '
+        logout_popup_active: ' ',
+        theme: 'light-theme '
     })
 
     var type_user = "User"
-    var theme = " light-theme"
 
     if (user.admin == 1) {
         type_user = "Admin"
     }
     if (user.theme == 1) {
-        theme = " dark-theme"
+        setClasses({
+            logout_popup_active: classes.logout_popup_active,
+            theme: 'dark-theme '
+        })
     }
 
     function logoutPopup() {
         if (classes.logout_popup_active === " ") {
-            setClasses({logout_popup_active: " active"})
+            setClasses({
+                logout_popup_active: " active",
+                theme: classes.theme
+            })
         } else {
-            setClasses({logout_popup_active: " "})
+            setClasses({
+                logout_popup_active: " ",
+                theme: classes.theme
+            })
         }
     }
 
@@ -44,80 +53,91 @@ function NavBar() {
         supState()
         navigate("/")
     }
+    console.log(user.username)
 
-    return(
-        <>
-        <div className="sidebar ligth-theme">
-            <div className="logo-details">
-                <i className='bx bxs-file-txt'></i>
-                <span className="logo_name">Untilted.txt</span>
+    if(!user.username){
+        return(
+            <>
+            <div>
+                <p>You are not loged</p>
             </div>
-            <ul className="nav-links">
-                <NavBarLink 
-                    name="Home" 
-                    img="bx bxs-grid" 
-                    link="/home" 
-                ></NavBarLink>
-                <NavBarLink 
-                    name="Forums" 
-                    img="bx bxs-collection"  
-                    link="/home/forum" 
-                ></NavBarLink>
-                <NavBarLinkSubMenu 
-                    name="Apps" 
-                    img="bx bx-cart-download" 
-                    subsMenus="Browse-Publish" 
-                    link="/apps"
-                ></NavBarLinkSubMenu>
-                <NavBarLinkSubMenu 
-                    name="Repport" 
-                    img="bx bxs-megaphone" 
-                    subsMenus="A bug-An good idea" 
-                    link="/repports"
-                ></NavBarLinkSubMenu>
+            </>
+        )
+    } else {
+        return(
+            <>
+            <div className="sidebar ligth-theme">
+                <div className="logo-details">
+                    <i className='bx bxs-file-txt'></i>
+                    <span className="logo_name">Untilted.txt</span>
+                </div>
+                <ul className="nav-links">
+                    <NavBarLink 
+                        name="Home" 
+                        img="bx bxs-grid" 
+                        link="/home" 
+                    ></NavBarLink>
+                    <NavBarLink 
+                        name="Forums" 
+                        img="bx bxs-collection"  
+                        link="/home/forum" 
+                    ></NavBarLink>
+                    <NavBarLinkSubMenu 
+                        name="Apps" 
+                        img="bx bx-cart-download" 
+                        subsMenus="Browse-Publish" 
+                        link="/apps"
+                    ></NavBarLinkSubMenu>
+                    <NavBarLinkSubMenu 
+                        name="Repport" 
+                        img="bx bxs-megaphone" 
+                        subsMenus="A bug-An good idea" 
+                        link="/repports"
+                    ></NavBarLinkSubMenu>
 
-                <li>
-                    <div className="profil">
-                        <div className="profil-content">
-                            <p className="pp-status">{user.pp_status}</p>
-                            <a className="preferences">
-                                <img src="/Ressources/img/profil-default.png" alt="profile" className="pp_img"/>
-                                <img src="/Ressources/img/engrenage_parametre.png" alt="preferences" className="preferences_engrenage"/>
-                            </a>
-                        </div>
-                        <div className="info">
-                            <div className="profil-name">{user.username}</div>
-                            <div className="type-user">
-                                <a href="/dashboard" className="dashboard">
-                                    {type_user}
+                    <li>
+                        <div className="profil">
+                            <div className="profil-content">
+                                <p className="pp-status">{user.pp_status}</p>
+                                <a className="preferences">
+                                    <img src="/Ressources/img/profil-default.png" alt="profile" className="pp_img"/>
+                                    <img src="/Ressources/img/engrenage_parametre.png" alt="preferences" className="preferences_engrenage"/>
                                 </a>
                             </div>
-                            <div className="theme-indicator">{user.theme}</div>
+                            <div className="info">
+                                <div className="profil-name">{user.username}</div>
+                                <div className="type-user">
+                                    <a href="/dashboard" className="dashboard">
+                                        {type_user}
+                                    </a>
+                                </div>
+                                <div className="theme-indicator">{user.theme}</div>
+                            </div>
+                            <div className="form">
+                                <div className="button_disconnect"><img src="/Ressources/img/log-out.png" onClick={logoutPopup}/></div>
+                            </div>
                         </div>
-                        <div className="form">
-                            <div className="button_disconnect"><img src="/Ressources/img/log-out.png" onClick={logoutPopup}/></div>
-                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div className={classes.theme}>
+                <Outlet></Outlet>
+            </div>
+            <div className={"overlay " + classes.theme + classes.logout_popup_active}></div>
+            <div className={"deco_popup " + classes.theme + classes.logout_popup_active}>
+                <div className="content">
+                    <p>Do you want to be disconnected ?</p>
+                    <div className="buttons">
+                        <form>
+                            <button type="submit" onClick={logout}>Disconnect</button>
+                        </form>
+                        <div className="close_popup" onClick={logoutPopup}>Close</div>
                     </div>
-                </li>
-            </ul>
-        </div>
-        <div className="light-theme">
-            <Outlet></Outlet>
-        </div>
-        <div className={"overlay" + theme + classes.logout_popup_active}></div>
-        <div className={"deco_popup" + theme + classes.logout_popup_active}>
-            <div className="content">
-                <p>Do you want to be disconnected ?</p>
-                <div className="buttons">
-                    <form>
-                        <button type="submit" onClick={logout}>Disconnect</button>
-                    </form>
-                    <div className="close_popup" onClick={logoutPopup}>Close</div>
                 </div>
             </div>
-        </div>
-        </>
-    )
+            </>
+        )
+    }
 }
 
 export default NavBar
