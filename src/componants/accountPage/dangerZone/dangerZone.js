f2ef5281b16abbb78c9914e7261b91b252a615c8
TheodoreDev <theodore.delbove@gmail.com>
2024-02-05 23:55:50 +0100

    delete user possibility

--- src/componants/accountPage/dangerZone/dangerZone.js
+++ src/componants/accountPage/dangerZone/dangerZone.js
@@ -1,15 +1,21 @@
 import React, {useEffect, useState} from "react";
 import './dangerZone.css'
-import { loadState } from "../../../helper/sessionStorage";
+import axios from 'axios'
+import { loadState, supState } from "../../../helper/sessionStorage";
+import { useNavigate } from 'react-router-dom';
 
 function DangerZone() {
     const sessionkey1 = "user"
     const user = loadState(sessionkey1, [0])
+    const navigate = useNavigate()
 
     const [classes, setClasses] = useState({
       button_delete : "active",
       form_delete : " "
     })
+    const [username, setUsername] = useState(user.username)
+    const [password_to_delete, setPassword] = useState('')
+    const [verify_char, setVerifyChar] = useState('')
 
     function deleteAccountShow() {
       if(classes.button_delete === "active") {
@@ -25,17 +31,34 @@ function DangerZone() {
       }
     }
 
+    function deleteAccount(event) {
+      event.preventDefault()
+      axios.post('http://localhost:5000/delete-user', {username, password_to_delete, verify_char})
+      .then(res => {
+        if(res.data === "Delete user") {
+          supState()
+          navigate('/')
+          alert("User delete")
+        } else {
+          navigate('/account')
+        }
+      })
+      .catch(error => console.log(error))
+    }
+
     return(
         <>
             <div className="delete-account">
                 <h3>⚠️ Delete your account</h3>
                 <button className={"button_delete " + classes.button_delete} onClick={deleteAccountShow}>DELETE</button>
-                <form className={classes.form_delete}>
+                <form className={classes.form_delete} onSubmit={deleteAccount}>
                     <img src="/Ressources/img/x-circle.svg" className="cancel-delete" onClick={deleteAccountShow}/>
                     <div className="data-to-delete">
-                        <input type="password" name="password" placeholder="password" className="delete-input" required></input>
+                        <input type="password" name="password" placeholder="password" className="delete-input" 
+                        onChange={e => setPassword(e.target.value)} required></input>
                         <p>Please type {user.username + "/" + user.email}</p>
-                        <input type="text" name="key_sentense" className="delete-input sentence" required></input>
+                        <input type="text" name="key_sentense" className="delete-input sentence" 
+                        onChange={e => setVerifyChar(e.target.value)} required></input>
                     </div>
                     <button type="submit" className="confirm-delete"><img src="/Ressources/img/trash.svg"></img></button>
                 </form>
